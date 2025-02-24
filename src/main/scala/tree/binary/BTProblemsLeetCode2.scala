e2cb6ffac4dec8a5683bc735a5bd433c6f442727
Kyle Van Wie <kvanwie87@gmail.com>
2025-02-24 14:26:31 -0500

    More BT Leetcode Problems

--- src/main/scala/tree/binary/BTProblemsLeetCode2.scala
+++ src/main/scala/tree/binary/BTProblemsLeetCode2.scala
@@ -41,4 +41,49 @@ object BTProblemsLeetCode2 {
 
     helper(List((root, 1)), 0)
   }
+
+  // https: //leetcode.com/problems/balanced-binary-tree/?envType=problem-list-v2&envId=binary-tree
+  def isBalanced(root: TreeNode): Boolean = {
+    def helper(curr: TreeNode): (Boolean, Int) = {
+      if (curr == null) (true, 0)
+      else {
+        val (leftIsBalanced, leftHeight) = helper(curr.left)
+        val (rightIsBalanced, rightHeight) = helper(curr.right)
+        (leftIsBalanced && rightIsBalanced && Math.abs(leftHeight - rightHeight) < 2, Math.max(leftHeight, rightHeight) + 1)
+      }
+    }
+    helper(root)._1
+  }
+
+  def isBalancedConcise(root: TreeNode): Boolean = {
+    def helper(curr: TreeNode): Int = { // information can be captured in just an Int by using -1 to mean unbalanced
+      if (curr == null) 0
+      else {
+        val leftHeight = helper(curr.left)
+        val rightHeight = helper(curr.right)
+        if(leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) -1 else Math.max(leftHeight, rightHeight) + 1
+      }
+    }
+    helper(root) != -1
+  }
+
+  def isBalancedTailrec(root: TreeNode): Boolean = {
+    @tailrec
+    def helper(stack: List[(TreeNode, Int, Int)], currHeight: Int): Int = {
+      if (stack.isEmpty) currHeight
+      else if (stack.head._1 == null) helper(stack.tail, 0)
+      else {
+        val (curr, visits, lHeight) = stack.head // node, visit count and left height stored in stack
+        if (visits == 0) { // first visit increase the visit count and push the left child
+          helper((curr.left, 0, 0) :: (curr, 1, 0) :: stack.tail, currHeight + 1)
+        } else if(visits == 1){ // second visit then currHeight value should be the height of the left sub tree, push the right child, increase visit count and set left height
+          helper((curr.right, 0, 0) :: (curr, 2, currHeight) :: stack.tail, currHeight)
+        } else { // third visit then currHeight should be the height of the right sub tree
+          if(lHeight == -1 || currHeight == -1 || Math.abs(lHeight - currHeight) > 1) -1
+          else helper(stack.tail, Math.max(lHeight, currHeight) + 1)
+        }
+      }
+    }
+    helper(List((root, 0, 0)), 0) != -1
+  }
 }
